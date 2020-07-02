0fffc3abd121e49b8e91cd5d9fe6c2986784c7ae
CodingRook <>
2020-07-02 22:15:22 +0100

    Created a temporary parent class for MovingPlatforms and copied over the linked actors of their PuzzleComponent to a TArray placeholder (via an additional component: ActorListComponent).
    NEXT TASK: delete the puzzle component of BP_MovingPlatform and reparent class so that the PuzzleComponent is now inherited from C++.

---
+++ Source/GameDevTVJam/MovingPlatform_temp.cpp
@@ -0,0 +1,42 @@
+// Game developed by CodingRook for GameDev.TV's 2020 Game Jam. All rights reserved.
+
+
+#include "MovingPlatform_temp.h"
+#include "ActorListComponent.h"
+#include "PuzzleComponent.h"
+
+// Sets default values
+AMovingPlatform_temp::AMovingPlatform_temp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ActorListComponent = CreateDefaultSubobject<UActorListComponent>("Actor List");
+
+}
+
+void AMovingPlatform_temp::PostLoad()
+{
+	Super::PostLoad();
+
+	TInlineComponentArray<UPuzzleComponent*> PuzzleComponent;
+	GetComponents(PuzzleComponent);
+
+	UE_LOG(LogTemp, Warning, TEXT("Found %i PuzzleComponents in %s"), PuzzleComponent.Num(), *PuzzleComponent[0]->GetName())
+	ActorListComponent->ActorList = PuzzleComponent[0]->GetActorsList();
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform_temp::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMovingPlatform_temp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
